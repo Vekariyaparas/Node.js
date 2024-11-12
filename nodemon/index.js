9ad5d4c62451877d42a51c69df93d5521dca2ba3
Paras Vekariya <113162042+Vekariyaparas@users.noreply.github.com>
2024-11-12 12:16:22 +0530

    Add files via upload

---
+++ nodemon/index.js
@@ -0,0 +1,14 @@
+const colors = require("colors")
+
+console.log("Node js code".green);
+
+
+
+
+
+
+//What is nodemon
+    // npm i nodemon -g
+    //nodemon is a tool that helps develop Node.js applications by automatically restarting the server when file changes are detected in your project directory. It saves time by   allowing you to see changes immediately without having to manually stop and restart the server.
+
+// => Node js is async language.
